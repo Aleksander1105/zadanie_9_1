6b81d34c8f7cc359f60972df97e3e2effa1f6e03
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-02-28 23:06:01 +0100

    Finished-zadanie_9_1

---
+++ script.js
@@ -0,0 +1,14 @@
+function getTriangleArea(a, h) {
+
+	if (a > 0 && h > 0) {
+	  return a*h/2;
+	} else {
+		return "Nieprawidłowe dane";
+	}
+};
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(20, 25);
+var triangle3Area = getTriangleArea(30, 35);
+
+console.log(triangle1Area);
